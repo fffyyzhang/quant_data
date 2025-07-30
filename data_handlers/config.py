8c064f09d9e508ad790046e6a18bf56dcd0786b3
liyuan <liyuan@liyuandeMacBook-Air.local>
2025-07-30 08:50:19 +0800

    修改文件下载形式

--- data_handlers/config.py
+++ data_handlers/config.py
@@ -1,2 +1,13 @@
-#DIR_DATA='/Users/liyuan/data/quant/raw'     #macbook开发机
-DIR_DATA='/data/data_liy/quant/raw'   #深圳.33服务器
+import os
+import subprocess
+
+try:
+    host = subprocess.getoutput('host $(hostname)')
+    if 'mac' in host.lower():
+        DIR_DATA='/Users/liyuan/data/quant/raw'
+    else:
+        DIR_DATA='/data/data_liy/quant/raw'
+except Exception:
+    DIR_DATA='/Users/liyuan/data/quant/raw'
+
+print(DIR_DATA)
